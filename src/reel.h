867352458a0ab42c9a39ece3b2c60242dc65387a
bougoumasarah <s.bougouma@epsi.fr>
2018-10-17 11:55:59 +0200

    commit 3

--- src/reel.h
+++ src/reel.h
@@ -4,11 +4,11 @@
 #pragma once
 using namespace std;
 /*!
-*\Declaration de la class Nombre
+*\Declaration de la class Nombre qui va servire a effectuer l'insertion; la suppression 
 */
 class nombre{
 public:
-/*declaration du float reel
+/* declaration du float reel
 */
 float reel;
 };
